6e892b953a6d6585384e417499032e607bdffa56
Anirban Hazra <Anirban.Hazra@aircommunities.com>
2026-07-22 18:01:30 +0530

    Settings and Sync icons added in header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Platform } from 'react-native';
+import { View, Text, StyleSheet, Image, Platform, Dimensions, TouchableOpacity } from 'react-native';
 import { Colors, PANTONE5487, PANTONE7546 } from '../constants/Colors';
 import { assets } from './Assets';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { headerTitle } from '../constants/Constants';
+import { SvgXml } from 'react-native-svg';
+import { settings, sync } from './Icons';
 
 interface AppHeaderProps {
   title: string;
@@ -16,9 +18,25 @@ const AppHeader: React.FC<AppHeaderProps> = ({ title, showLogo = true }) => {
 
   return (
     <View style={[styles.outerContainer, { paddingTop: insets.top }]}>
+      {/* Screen Header */}
       <View style={styles.innerContainer}>
         <View style={styles.blankView} />
         <Text style={styles.titleText}>{headerTitle}</Text>
+      </View>
+      <View style={styles.iconsContainer}>
+        {/* Settings */}
+        <TouchableOpacity
+          activeOpacity={0.7}
+        >
+          <SvgXml xml={settings} color={Colors.white} height={20} width={20} />
+        </TouchableOpacity>
+        {/* Offline Sync Icon */}
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={{ marginLeft: 10 }}
+        >
+          <SvgXml xml={sync} color={Colors.white} height={23} width={23} />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -31,14 +49,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: PANTONE7546,
     width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 20
   },
   innerContainer: {
     padding: 10,
-    flex: 1,
+    flex: 0.98,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 15,
   },
   titleText: {
     color: Colors.white,
@@ -76,6 +95,13 @@ const styles = StyleSheet.create({
     height: 25,
     width: 25,
   },
+  iconsContainer: {
+    position: 'absolute',
+    right: 10,
+    top: Dimensions.get('window').height * 0.085,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
 
 export default AppHeader;
